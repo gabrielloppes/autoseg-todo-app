aa459530067ecf44a43120c5c4ad5a35f5446cae
Gabriel Lopes <gabriellopees@hotmail.com>
2020-10-25 18:03:23 -0300

    Ability do delete an item, and validates content of item

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -6,6 +6,17 @@ class TodoItemsController < ApplicationController
     redirect_to @todo_list
   end
 
+  # I can delete a specific to-do item, if all items be deleted the whole list will also be destroyed
+  def destroy 
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.destroy
+      flash[:success] = "Todo item was destroyed"
+    else
+      flash[:error] = "Todo item could not be deleted"
+    end
+    redirect_to @todo_list
+  end
+
   private
 
   def set_todo_list
--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,3 +1,5 @@
 class TodoItem < ApplicationRecord
   belongs_to :todo_list
+  # Only creates a todo-item if the content exists
+  validates :content, presence: true
 end
