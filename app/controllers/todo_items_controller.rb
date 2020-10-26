ec6753c81712257e6ea78c277da56b9de3c9b8c8
Gabriel Lopes <gabriellopees@hotmail.com>
2020-10-26 03:23:16 -0300

    Added new views and style

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,6 +1,8 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
+  before_action :set_todo_item, except: [:create]
   # I can create a to-do item inside a list with a set of trusted paramters
+  
   def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
     redirect_to @todo_list
@@ -17,7 +19,16 @@ class TodoItemsController < ApplicationController
     redirect_to @todo_list
   end
 
+  def complete
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: "Todo item completed"
+  end
+
   private
+
+  def set_todo_item 
+    @todo_item = @todo_list.todo_items.find(params[:id])
+  end
 
   def set_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
